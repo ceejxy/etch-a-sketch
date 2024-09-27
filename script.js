8b5f6f01642fa42dd270e80904b0cfd02f651ab4
Clark G <136202713+ceejxy@users.noreply.github.com>
2024-09-26 22:30:07 -0400

    added event listener to turn tiles gray when mouse goes over. added event listener to turntile white if clicked. added button to reset tile color to white when clicked

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ const container = document.querySelector('#container');
 
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
+    square.innerText = i
     square.classList.add('squares');
     square.style.color = 'red';
     container.appendChild(square);
 }
 
+
+container.addEventListener('mouseover', function(e) {
+   e.target.style.backgroundColor = 'gray';
+})
+
+container.addEventListener('click', function(e) {
+    e.target.style.backgroundColor = 'white';
+ })
+
+ const button = document.createElement('button');
+ button.innerText = 'Reset Tiles'
+ document.body.appendChild(button)
+
+ button.addEventListener('click', function(e) {
+    container.style.backgroundColor = 'white';
+ })
+
+ 
+
+
+ 
 
 
