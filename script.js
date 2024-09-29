8486db513b08c46cad842cebc76bbc47ee263d0e
Clark G <136202713+ceejxy@users.noreply.github.com>
2024-09-29 18:39:45 -0400

    added button to remove the original 16x16 grid and takes in a input from the user to determine the dimensions of the new grid and then add the new grid to the page

--- script.js
+++ script.js
@@ -1,31 +1,82 @@
-const container = document.querySelector('#container');
+const defaultContainer = document.querySelector('#container');
 
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
-    square.innerText = i
     square.classList.add('squares');
     square.style.color = 'red';
-    container.appendChild(square);
+    defaultContainer.appendChild(square);
+
+    square.addEventListener('mouseover', function(e) {
+         e.target.style.backgroundColor = 'gray';
+   }) 
 }
 
-
-container.addEventListener('mouseover', function(e) {
-   e.target.style.backgroundColor = 'gray';
-})
-
 container.addEventListener('click', function(e) {
+   if (e.target.style.backgroundColor === 'gray') {
     e.target.style.backgroundColor = 'white';
- })
+ }})
 
- const button = document.createElement('button');
- button.innerText = 'Reset Tiles'
- document.body.appendChild(button)
 
- button.addEventListener('click', function(e) {
-    container.style.backgroundColor = 'white';
- })
-
+ // new container created depending on user input
  
+const button = document.createElement('button');
+button.innerText = 'Change number of Squares'
+document.body.insertBefore(button, document.body.firstChild);
+
+const newContainer = document.querySelector('#newContainer')
+
+
+button.addEventListener('click', function(e) {
+
+   defaultContainer.remove();
+
+   userInput = prompt('enter dimensions for new grid');
+   newNumber = parseInt(userInput)**2;   
+
+   for (let i = 0; i < newNumber; i++) {
+      const newSquare = document.createElement('div');
+      newSquare.classList.add('newSquares');
+      newSquare.style.color = 'red';
+      newContainer.appendChild(newSquare);
+
+      newSquare.addEventListener('mouseover', function(e) {
+         e.target.style.backgroundColor = 'gray';
+      }) 
+  } 
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function newGrid(gridNumber) {
+
+   userInput = prompt('enter any number below 100')
+   gridNumber = parseInt(userInput)
+
+   if (gridNumber <= 100) {
+      for (let i = 0; i < gridNumber; i++) {
+         const square = document.createElement('div');
+         square.classList.add('squares');
+         square.style.color = 'red';
+         defaultContainer.appendChild(square); 
+     }
+   }
+}*/ 
+
+
+
+
+
 
 
  
